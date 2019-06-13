e0754b11fba7ab4953fd359b6bebdd94be1a6ce4
ktamulonis <kurttamulonis@gmail.com>
2019-06-13 10:44:39 -0400

    creates Home controller, index view, and passing feature spec sets home#index as root add's some style to home index page

---
+++ spec/features/visiting_home_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature "Visiting Home Index" do
+  
+  scenario "a guest see's the home page first" do
+    visit root_url
+    expect(page.has_text?("Welcome to Build a Buddy")).to eql(true)
+  end
+
+end
