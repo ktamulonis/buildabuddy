f00d121c9bc568c05ae92d0e30daffd479f3b3f5
ktamulonis <kurttamulonis@gmail.com>
2019-06-13 10:00:09 -0400

    completes unit tests for items add's required validations in model to pass item specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Item, type: :model do
   it "has a valid factory" do 
   	expect(create :item).to be_valid
   end
+
+  it "must have a description" do
+    expect(build :item, description: nil).to_not be_valid
+  end
+
+  it "must have a quantity" do
+    expect(build :item, quantity: nil).to_not be_valid
+  end
+
+  it "doesn't need to have a size" do
+    expect(create :item, size: nil).to be_valid
+  end
+
+  it "doesn't need to have a cost" do
+    expect(create :item, cost: nil).to be_valid
+  end
+
+  it "doesn't need to have a sales price" do
+    expect(create :item, sales_price: nil).to be_valid
+  end
 end
